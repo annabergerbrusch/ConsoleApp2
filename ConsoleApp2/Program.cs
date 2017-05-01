bf767d6f7cd390f8b5af2d909cec4390fa4c162d
annabergerbrusch <annaberger@live.dk>
2017-05-01 15:29:42 +0200

    Første

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Indtast navn");
+            string navn = Console.ReadLine();
+
+            string uppernavn = navn.ToUpper();
+            Console.WriteLine("Velkommen" + " " + uppernavn);
+            Console.WriteLine("Tryk en tast for at fortsætte");
+            Console.ReadKey();
+        }
+    }
+}
