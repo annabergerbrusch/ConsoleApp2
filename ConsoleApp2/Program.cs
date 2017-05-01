3536a2465a35a48abb870af3a0aacf6810ee5b29
annabergerbrusch <annaberger@live.dk>
2017-05-01 15:30:41 +0200

    første

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,11 +10,8 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-
-
             Console.WriteLine("Indtast navn");
             string navn = Console.ReadLine();
-
             string uppernavn = navn.ToUpper();
             Console.WriteLine("Velkommen" + " " + uppernavn);
             Console.WriteLine("Tryk en tast for at fortsætte");
